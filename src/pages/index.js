d2c6dd1709da5d1ef3d51688b427538d0f45a5ce
megazear7 <alexlockahrt7@gmail.com>
2023-02-19 22:24:51 -0500

    Added attribution

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,4 +6,5 @@ export default context => html`
   <img class="game-icon-left" src="/icons/icon-128x128.png">
   <img class="game-icon-right" src="/icons/icon-128x128.png">
   <gongga-game></gongga-game>
+  <a class="attribution" href="https://www.flaticon.com/free-icons/ping-pong" title="ping pong icons">Ping pong icons created by Freepik - Flaticon</a>
 `;
